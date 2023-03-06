9695a3f529c1a22af570aaae79f83645c168e346
poritsal <121952648+poritsal@users.noreply.github.com>
2023-03-06 12:51:14 +0300

    Update quad.h

--- quad.h
+++ quad.h
@@ -4,10 +4,6 @@
 #include <vector>
 #include <fstream>
 
-//void safe_open(std::ifstream& file);
-//void safe_out(std::ofstream& file);
-//std::vector <double> parser(std::ifstream& file);
-//void quad_out(int r, double a, double b, double c);
 void quad_sol();
 
 # endif 
